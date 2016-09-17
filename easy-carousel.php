05e0d0005365b64de53ddf3f292ce4da384292a4
vargavd <vvd007@gmail.com>
2016-09-17 10:55:24 +0200

    Add QU String lib + some logic on galleries.

--- easy-carousel.php
+++ easy-carousel.php
@@ -470,83 +470,96 @@ function ec_admin_menu_page() {
         <p class="description">Here, you can define galleries, which are collections of images. When you insert a slideshow (with shortcode, php or widget), you must define the id of the shown gallery. </p>
 
         <button class="add gallery-button"> + Add Gallery </button>
-        <div class="gallery-template">
+        
+        <div class="gallery-elem gallery-template" data-id="0">
             <div class="gallery-header">
-                <button class="add gallery-button"> + Add Image </button>
+                <button class="add image-button"> + Add Image </button>
                 <button class="delete gallery-button" type="button"> X Delete Gallery </button>
                 ID: <input type="text" class="header-id" placeholder="gallery-1" /> 
             </div>
             <div class="gallery-images">
+                <div class="gallery-image-wrapper gallery-image-template" data-id="">
+                    <img src="" />
+                    <div class="img-button-wrapper">
+                        <div class="img-button-wrapper">
+                            <button class="delete">X</button>                  
+                        </div>
+                    </div>
+                </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/10/bridgefoy.jpg" />
                     <div class="img-button-wrapper">
-                        <div class="img-button-wrapper">                      <button class="delete gallery-button">X</button>                  </div>
+                        <div class="img-button-wrapper">
+                            <button class="delete">X</button>                  
+                        </div>
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/08/bakery-backdrop1.jpg" />
                     <div class="img-button-wrapper">
-                        <div class="img-button-wrapper">                      <button class="delete gallery-button">X</button>                  </div>
+                        <div class="img-button-wrapper">
+                            <button class="delete">X</button>                  
+                        </div>
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/08/cheesecake.png" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/12/construction.jpg" />
                     <div class="img-button-wrapper">
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/08/corporatevent.jpg" />
                     <div class="img-button-wrapper">
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/slidea2.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/06.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/parallax.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/8.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/d.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/09/a1.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
                 <div class="gallery-image-wrapper">
                     <img src="http://takacsb.dev/wp-content/uploads/2015/08/comingsoonback.jpg" />
                     <div class="img-button-wrapper">                      
-                        <button class="delete gallery-button">X</button>                  
+                        <button class="delete">X</button>                  
                     </div>
                 </div>
             </div>
--- js/ec-admin.js
+++ js/ec-admin.js
@@ -21,11 +21,16 @@ jQuery(document).ready(function ($) {
     }
     
     function handleGalleries() {
-        var $getImgButton = $(".select-image"),
-            $imgTitle     = $(".image-title"),
-            $imgUrl       = $(".image-url"),
-            $imgId        = $(".image-id"),
+        var 
+            // $elems
+            $addGalleryButton = $("button.add.gallery"),
+            $galleryTemplate  = $(".gallery-template"),
+            $currGallery,
             
+            // misc
+            galleriesCount = 0,
+            
+            // wp media modal window
             frame = wp.media({
                 title: 'Select or Upload Media Of Your Chosen Persuasion',
                 multiple: false,
@@ -36,6 +41,60 @@ jQuery(document).ready(function ($) {
                     type: 'image'
                 }
             });
+    
+        function $getGallery(info) {
+            var $gallery;
+            
+            // helper functions
+            function getGallery() {
+                
+                // if info is the id
+                if (typeof info === 'number') {
+                    $gallery = $('.gallery[data-id="' + info + '"]');
+                } else if (info instanceof jQuery) { // if info is a DOM element
+                    
+                    if (!info.hasClass('gallery')) {
+                        
+                        $gallery = $gallery.closest('.gallery'); // info must be a child of the .gallery
+                        
+                        if ($gallery.length === 0) {
+                            // no .gallery found in parents tree
+                            throw new Exception('No gallery found.');
+                        }
+                    }
+                } else { // otherwise we make a new gallery
+                    $gallery = $galleryTemplate.clone().removeClass('gallery-template');
+                }
+                
+            }
+            function getImages() {
+                 return $gallery.find('.gallery-image-template:not(.gallery-image)');
+            }
+            
+            // get $gallery based in info parameter
+            getGallery();
+            
+            return {
+                id: $gallery.attr('data-id'),
+                gallery: $gallery,
+                addImgButton: $gallery.find('button.add.image-button'),
+                deleteImgButton: $gallery.find('button.delete.image-button'),
+                deleteGalleryButton: $gallery.find('button.delete.gallery-button'),
+                imgTemplate: $gallery.find('.gallery-image-template'),
+                getImgs: getImages
+                
+            };
+        }
+        function $getImage(id) {
+            var $imgWrapper = $('.gallery-image-wrapper[data-id="' + id + '"]');
+            
+            return {
+                id: id,
+                imgWrapper: $imgWrapper,
+                img: $imgWrapper.find('img'),
+                deleteButton: $imgWrapper.find('button.delete')
+            };
+        }
     
         function getImageClicked() {
             frame.open();
@@ -49,10 +108,14 @@ jQuery(document).ready(function ($) {
             $imgId.text(image.id);
         }
         
+        // set events on the gallery template
+        $currGallery = $getGallery($galleryTemplate);
+        
+        
         frame.on('select', imageSelected);
         
-        $getImgButton.click(getImageClicked);
-    }
+        
+   }
     
     handleTabs();
     handleGalleries();
